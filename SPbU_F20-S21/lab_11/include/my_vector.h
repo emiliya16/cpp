8f39fea14f779c3801e9b75ee9945fa01cfe5a27
Эмилия <71070803+emiliya16@users.noreply.github.com>
2022-12-18 22:22:17 +0300

    Update my_vector.h

--- SPbU_F20-S21/lab_11/include/my_vector.h
+++ SPbU_F20-S21/lab_11/include/my_vector.h
@@ -1,36 +1,46 @@
 #ifndef MY_VECTOR_H_
 #define MY_VECTOR_H_
 
+#pragma once
+
 #include <cstddef>
+#include <ostream>
+#include <memory>
+#include <type_traits>
+#include <initializer_list>
 
 namespace containers {
 
 template<typename T>
 class my_vector {
 public:
-    my_vector();
-    my_vector(std::size_t n);
-    my_vector(my_vector other);
-    my_vector operator=(my_vector other);
+    my_vector(std::size_t n=0);
+    my_vector(std::initializer_list<T> l);
+    my_vector(containers::my_vector<T> const &other);
+    void operator=(containers::my_vector<T> const & other);
     ~my_vector();
 
-    std::size_t size();
-    std::size_t capacity();
+    volatile std::size_t size();
+    volatile std::size_t capacity();
     bool empty();
 
     void resize(std::size_t n);
     void reserve(std::size_t n);
 
-    ?? operator[](std::size_t index);
+    T & operator[](std::size_t index);
 
     void push_back(T t);
-    void pop_back();
+    T pop_back();
     void clear();
 
+    template <typename U>
+    friend std::ostream & operator<<(std::ostream &stream, const containers::my_vector<U> & v);
+
 private:
-    size_t capacity_;
-    size_t size_;
-    T* array_;
+    std::size_t size_ = 0;
+    std::size_t capacity_ = 0;
+    T * array_;
+    std::allocator<T> allocator_;
 };
 
 }

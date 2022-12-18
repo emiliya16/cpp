fa63bb13e67184f7e7a517380525aab177ea02cd
Эмилия <71070803+emiliya16@users.noreply.github.com>
2022-12-18 22:32:42 +0300

    Update Circle.hpp

--- SPbU_F20-S21/lab_09/include/Circle.hpp
+++ SPbU_F20-S21/lab_09/include/Circle.hpp
@@ -1,10 +1,5 @@
-#pragma once
-
-#include "Figure.hpp"
-
-class Circle : public Figure {
-public:
-    Circle(int id, int x, int y, int radius, const char* label);
-
-    // ...
-};
+100
+0
+0
+10
+"Круг"

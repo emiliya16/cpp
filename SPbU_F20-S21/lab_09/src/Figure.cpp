24945b3b04c18f77565fba0bf25dcd65b78db747
Эмилия <71070803+emiliya16@users.noreply.github.com>
2023-01-12 14:42:09 +0300

    Update Figure.cpp

--- SPbU_F20-S21/lab_09/src/Figure.cpp
+++ SPbU_F20-S21/lab_09/src/Figure.cpp
@@ -1,3 +1,17 @@
-#include "Figure.hpp"
+#include "../include/Figure.hpp"
 
-// TODO
+#include <cstdio>
+ 
+Figure::Figure(int _x, int _y):
+	m_x(_x), m_y(_y)
+{
+	m_id = total_++;
+    printf("Add figure ");
+}
+
+void Figure::move(int new_x, int new_y)
+{
+	m_x = new_x;
+	m_y = new_y;
+	printf("move figure id %d\n", m_id);
+}

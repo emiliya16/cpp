a338cf55d12cfbc73d764999a3f6046eb2d4effe
Эмилия <71070803+emiliya16@users.noreply.github.com>
2023-01-12 14:42:51 +0300

    Update main.cpp

--- SPbU_F20-S21/lab_09/src/main.cpp
+++ SPbU_F20-S21/lab_09/src/main.cpp
@@ -1,57 +1,21 @@
-#include <iotream>
-#include <fstream>
-#include <graphics.h>
-#include <cmath>
-#include <ctime>
-#include <string.h>
-using namespace std;
-int main()
-{
-    int zoom_rectangle(int a, int b, int n)
-    {
-    	return a * n, b * n;
-    }
-    int zoom_circle(int r, int n)
-    {
-    	return r * n;
-    }
-    class Students {
-    ofstream sircle, digure, rectangle, scheme;
-    sircle.open("Circle.hpp");
-    digure.open("Figure.hpp");
-    rectangle.open("Rectangle.hpp");
-    scheme.open("Scheme.hpp");
-    
-    n = 2;
-    circle_id >> Sircle; 
-    circle_x >> sircle;
-    circle_y >> sircle;
-    r >> sircle;
-    sircle_s >> sircle;
-   
-    rectangle_id >> rectangle;
-    rectangle_x >> rectangle;
-    rectangle_y >> rectangle;
-    a >> rectangle;
-    b >> rectangle;
-    rectangle_s >> rectangle;
-    
-    a, b = zoom_rectangle(a, b, n);
-    r = zoom_sircle(r, n);
-    
-    setcolor (RGB(255,100,100));
-    rectangle(rectangle_x - a/2, rectangle_y - b/2, rectangle_x + a/2, rectangle_y + b/2);
-    circle(circle_x, cercle_y, r);
-    outtextxy(rectangle_x, rectangle_y, rectangle_s);
-    outtextxy(circle_x, circle_y, circle_s);
+#include "../include/Scheme.hpp"
 
-    sircle.close();
-    digure.close();
-    rectangle.close();
-    scheme.close();
-    system("pause");
-};
-    printf(circle_id, circle_x, circle_y, circle_r, sircle_s)
-    printf(rectangle_id, rectangle_x, rectangle_y, )
-    return 0;
+#include <iostream>
+
+int Figure::total_ = 0;
+
+int main()
+{	
+	Scheme *schem = new Scheme();
+	schem->add_rect(3, 3);
+	schem->add_rect(2, 2);
+	schem->add_circle(1, 1, "circle1");
+	
+	schem->move_figure(1, 5, 5);
+	
+	schem->print_figures();
+	
+	delete schem;
+	
+	return 0;
 }

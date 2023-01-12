b36019f067e9389b4f76568babe375fd5e450475
Эмилия <71070803+emiliya16@users.noreply.github.com>
2023-01-12 14:43:32 +0300

    Update Rectangle.cpp

--- SPbU_F20-S21/lab_09/src/Rectangle.cpp
+++ SPbU_F20-S21/lab_09/src/Rectangle.cpp
@@ -1,3 +1,34 @@
-#include "Rectangle.hpp"
+#include "../include/Rectangle.hpp"
 
-// TODO
+#include <cstdio>
+ 
+Rectangle::Rectangle(int _x, int _y) : Figure(_x, _y)
+{
+    printf("rectangle\n");
+}
+
+Rectangle::~Rectangle()
+{
+    printf("Delete rectangle id %d\n", m_id);
+}
+
+void Rectangle::print()
+{
+	printf("Rectangle %d: x = %d y = %d width = %d height = %d\n", m_id, m_x, m_y, width, height);
+}
+
+void Rectangle::zoom(int factor)
+{
+	width 	+= factor;
+	height	+= factor;
+	printf("zoom rect id %d\n", m_id);
+}
+
+bool Rectangle::is_inside(int x, int y)
+{
+	int right	= m_x + width/2;
+	int left	= m_x - width/2;
+	int bottom	= m_y + height/2;
+	int top		= m_y - height/2;
+	return (x <= right && x>= left && y<= bottom && y >= top);
+}

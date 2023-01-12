33f766aec3a53bacae8074d363f63ff208b04efb
Эмилия <71070803+emiliya16@users.noreply.github.com>
2023-01-12 14:44:15 +0300

    Update Scheme.cpp

--- SPbU_F20-S21/lab_09/src/Scheme.cpp
+++ SPbU_F20-S21/lab_09/src/Scheme.cpp
@@ -1,3 +1,38 @@
-#include "Scheme.hpp"
+#include "../include/Scheme.hpp"
 
-// TODO
+#include <cstdio>
+ 
+Scheme::Scheme()
+{
+    printf("Add scheme\n");
+}
+
+Scheme::~Scheme()
+{
+    printf("Delete scheme\n");
+    for( int i = 0; i < m_figures.size(); ++i )
+		delete m_figures[i];
+    m_figures.clear();
+}
+
+void Scheme::add_circle(int _x, int _y, std::string _label)
+{
+	m_figures.push_back( new Circle(_x, _y, _label) );
+}
+
+void Scheme::add_rect(int _x, int _y)
+{
+	m_figures.push_back( new Rectangle(_x, _y) );
+}
+
+void Scheme::print_figures()
+{
+	for(Figure* figure : m_figures)
+		figure->print();
+}
+
+void Scheme::move_figure(int _id, int _x, int _y)
+{
+	if( _id <= m_figures.size() )
+		m_figures.at(_id)->move(_x, _y);
+}

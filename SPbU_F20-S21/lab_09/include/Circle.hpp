c727bf0906f2d2a6d7abbb86512d44c9c1f78c32
Эмилия <71070803+emiliya16@users.noreply.github.com>
2023-01-12 14:38:25 +0300

    Update Circle.hpp

--- SPbU_F20-S21/lab_09/include/Circle.hpp
+++ SPbU_F20-S21/lab_09/include/Circle.hpp
@@ -1,5 +1,23 @@
-100
-0
-0
-10
-"Круг"
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+ 
+#include "Figure.hpp"
+
+#include <string>
+
+class Circle : public Figure
+{
+private:
+	std::string m_label = "";
+	int radius = 0;
+	
+public:
+	Circle(int _x, int _y, std::string _label);
+	~Circle();
+	
+	void print() override;
+	void zoom(int factor) override;
+	bool is_inside(int x, int y) override;
+};
+ 
+#endif

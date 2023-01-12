cbc51f8d4b7b9a274f7e29fbff458b22fe2d12c6
Эмилия <71070803+emiliya16@users.noreply.github.com>
2023-01-12 14:39:15 +0300

    Update Figure.hpp

--- SPbU_F20-S21/lab_09/include/Figure.hpp
+++ SPbU_F20-S21/lab_09/include/Figure.hpp
@@ -1,19 +1,22 @@
-#pragma once
+#ifndef FIGURE_HPP
+#define FIGURE_HPP
 
-class Figure {
+#include <cstddef>
+ 
+class Figure
+{
 public:
-    Figure(int id, int x, int y);
-    virtual ~Figure();
-
-    virtual void print() const = 0;
-    virtual bool is_inside(int x, int y) const = 0;
-    virtual void zoom(int factor) = 0;
-    void move(int new_x, int new_y);
-
-    // ...
-
+	Figure(int _x, int _y);
+	virtual ~Figure() = default;
+	virtual void print() {};
+	virtual void zoom(int factor) {};
+	virtual bool is_inside(int x, int y) {return true;}
+	void move(int new_x, int new_y);
 protected:
-    int id;
-    int x;
-    int y;
+    int m_id;
+    static int total_;
+    int m_x;
+    int m_y;
 };
+ 
+#endif

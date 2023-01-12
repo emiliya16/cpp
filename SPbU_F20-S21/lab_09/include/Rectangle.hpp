f2376fd99363e2edbe9b2d6d08b60c9d7d17eaf3
Эмилия <71070803+emiliya16@users.noreply.github.com>
2023-01-12 14:39:36 +0300

    Update Rectangle.hpp

--- SPbU_F20-S21/lab_09/include/Rectangle.hpp
+++ SPbU_F20-S21/lab_09/include/Rectangle.hpp
@@ -1,6 +1,20 @@
-200
-20
-20
-30
-40
-"Прямоугольник"
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+ 
+#include "Figure.hpp"
+
+class Rectangle : public Figure
+{
+	int width = 1;
+	int height = 1;
+	
+public:
+	Rectangle(int _x, int _y);
+	~Rectangle();
+	
+	void print() override;
+	void zoom(int factor) override;
+	bool is_inside(int x, int y) override;
+};
+ 
+#endif
